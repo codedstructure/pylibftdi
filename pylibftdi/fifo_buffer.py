907f582312ffcaa0a2cf042e130037780b72ddf6
Ben Bass <benbass@codedstructure.net>
2015-05-11 22:41:31 +0100

    add blocking extract to FifoBuffer

--- pylibftdi/fifo_buffer.py
+++ pylibftdi/fifo_buffer.py
@@ -5,6 +5,7 @@ bytestrings or text to and from a device
 @codedstructure 2015
 """
 
+import threading
 from collections import deque
 
 try:
@@ -13,26 +14,41 @@ except ImportError:
     from collections import Sized
 
 
-class FifoBuffer(deque):
-    joiner = b''
+class FifoBuffer(object):
+    """
+    A FIFO buffer for streaming bytes
+    """
 
-    def extract(self, length):
+    def __init__(self):
+        self._cond = threading.Condition()
+        self._deque = deque()
+
+    def extract(self, length, block=False):
         """
-        Read 'length' bytes from the fifo
+        Read upto 'length' bytes from the fifo
+
+        :param length: - number of bytes to read
+        :param block: - block waiting for any data to be ready
         """
         i = 0
         result = []
         while i < length:
-            if self:
-                item = self.popleft()
+            if self._deque:
+                item = self._deque.popleft()
                 i += len(item)
                 result.append(item)
             else:
-                break
-        result_str = self.joiner.join(result)
+                if block and not result:
+                    with self._cond:
+                        if not self._deque:
+                            # Use a timeout here to allow Ctrl-C interrupt
+                            self._cond.wait(1)
+                else:
+                    break
+        result_str = b''.join(result)
         result_str, excess = result_str[:length], result_str[length:]
         if excess:
-            self.appendleft(excess)
+            self._deque.appendleft(excess)
         return result_str
 
     def insert(self, data):
@@ -40,13 +56,12 @@ class FifoBuffer(deque):
         Insert given data string into the fifo
         """
         if isinstance(data, Sized):
-            self.append(data)
+            self._deque.append(data)
         else:
             raise ValueError("Sized sequences only")
 
+        with self._cond:
+            self._cond.notify()
+
     def __len__(self):
-        return sum(len(x) for x in self)
-
-
-class FifoTextBuffer(FifoBuffer):
-    joiner = u''
+        return sum(len(x) for x in self._deque)
