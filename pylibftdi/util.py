84d2eb503bf218c7a15d4a9daf6a0dc1144501e5
Ben Bass <benbass@codedstructure.net>
2015-05-21 23:13:08 +0100

    move fifo_buffer to pylibftdi.util, add test for it

--- pylibftdi/util.py
+++ pylibftdi/util.py
@@ -41,3 +41,68 @@ class Bus(object):
         val &= ~(self._mask << self.offset)
         val |= value << self.offset
         obj.device.port = val
+
+
+import threading
+from collections import deque
+
+try:
+    from collections.abc import Sized
+except ImportError:
+    from collections import Sized
+
+
+class FifoBuffer(object):
+    """
+    A FIFO buffer for streaming bytes
+
+    provides an unbounded fifo buffer for reading and writing
+    bytestrings or text to and from a device
+    """
+
+    def __init__(self):
+        self._cond = threading.Condition()
+        self._deque = deque()
+
+    def extract(self, length, block=False):
+        """
+        Read upto 'length' bytes from the fifo
+
+        :param length: - number of bytes to read
+        :param block: - block waiting for any data to be ready
+        """
+        i = 0
+        result = []
+        while i < length:
+            if self._deque:
+                item = self._deque.popleft()
+                i += len(item)
+                result.append(item)
+            else:
+                if block and not result:
+                    with self._cond:
+                        if not self._deque:
+                            # Use a timeout here to allow Ctrl-C interrupt
+                            self._cond.wait(1)
+                else:
+                    break
+        result_str = b''.join(result)
+        result_str, excess = result_str[:length], result_str[length:]
+        if excess:
+            self._deque.appendleft(excess)
+        return result_str
+
+    def insert(self, data):
+        """
+        Insert given data string into the fifo
+        """
+        if isinstance(data, Sized):
+            self._deque.append(data)
+        else:
+            raise ValueError("Sized sequences only")
+
+        with self._cond:
+            self._cond.notify()
+
+    def __len__(self):
+        return sum(len(x) for x in self._deque)
