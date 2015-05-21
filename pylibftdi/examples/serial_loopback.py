78af60364197cf8589ac3cae593da5f8c8bfa4e2
Ben Bass <benbass@codedstructure.net>
2015-05-21 22:32:20 +0100

    make serial_loopback bisect to determine max buffer size

--- pylibftdi/examples/serial_loopback.py
+++ pylibftdi/examples/serial_loopback.py
@@ -2,7 +2,7 @@
 """
 test serial loopback; assumes Rx and Tx are connected
 
-Copyright (c) 2010-2014 Ben Bass <benbass@codedstructure.net>
+Copyright (c) 2010-2015 Ben Bass <benbass@codedstructure.net>
 All rights reserved.
 """
 
@@ -10,13 +10,8 @@ import os
 import sys
 import time
 
-from pylibftdi import Device
+from pylibftdi import SerialDevice
 
-
-# TODO:
-# make this threaded - a writer stream, which sends known data with
-# occasional sync blocks, and a reader which reads and waits for a sync-block
-# then signals the writer if it gets stuck.
 
 def test_string(length):
     return os.urandom(length)
@@ -24,37 +19,51 @@ def test_string(length):
 
 class LoopbackTester(object):
     def __init__(self):
-        self.device = Device()
+        self.device = SerialDevice(chunk_size=16)
 
-    def test_loopback(self, lengths):
+    def test_loopback(self, l):
+        test_str = test_string(l)
+        if self.device.write(test_str) != len(test_str):
+            sys.stdout.write('*')
+        time.sleep(0.1)
+        result = ''
+        for _ in range(5):
+            result = self.device.read(l)
+            time.sleep(0.1)
+            if result:
+                break
+        if result != test_str:
+            self.device.flush()
+            time.sleep(0.25)
+        return result == test_str
+
+    def test_iter(self, lengths):
         self.device.flush()
         time.sleep(0.1)
         for l in lengths:
-            test_str = test_string(l)
-            if self.device.write(test_str) != len(test_str):
-                sys.stdout.write('*')
-            time.sleep(0.1)
-            result = ''
-            for _ in range(3):
-                result = self.device.read(l)
-                if result:
-                    break
-            if result != test_str:
-                self.device.flush()
-                time.sleep(0.25)
-            yield result == test_str
+            yield self.test_loopback(l)
+
+    def bisect(self):
+        xmin, xmax = 1, 5000
+        last_test = None
+        while True:
+            test = (xmin + xmax) // 2
+            if test == last_test:
+                break
+            if self.test_loopback(test):
+                xmin = test
+            else:
+                xmax = test
+            last_test = test
+        return test
 
     def main(self):
+        print("Determining largest non-streamed buffer size")
         for bd in [9600, 31250, 115200, 1152000]:
+            print("Baudrate: {}".format(bd))
             self.device.baudrate = bd
-
-            for result in self.test_loopback(range(1, 50) + range(100, 500, 100) +
-                                             range(1000, 5000, 1000)):
-                if result:
-                    sys.stdout.write('+')
-                else:
-                    sys.stdout.write('!')
-            sys.stdout.write('\n')
+            result = self.bisect()
+            print("Buffer size: {}".format(result))
 
 
 if __name__ == '__main__':
